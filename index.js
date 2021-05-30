4c48bc59017c317cee8ae01faecc692ed8764446
Aditya Pratap <adityapratap7344@gmail.com>
2021-05-30 12:01:49 +0530

    minor update

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const { initaliazeServer } = require('./src/server');
 const main = async () => {
     const server = await initaliazeServer();
 
-    process.on('unhandledRejection', (/** @type {Error} */err, _promise) => {
+    process.on('unhandledRejection', (/** @type {Error} */ err, _promise) => {
         console.log(chalk.red(`Error: ${err.message}`));
         server.close(() => process.exit(1));
     });
-}
+};
 
+// start server here!
 main();
